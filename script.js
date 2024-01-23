3e2440ea5dd3c0e2c6d8739762cf7c64bc9a4afb
Antimio <antimiocasimiro@gmail.com>
2024-01-23 23:12:24 +0000

    Created a function to display the updated date and time

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ var timeDisplayEl = $("#currentDay"); //Capture the p element with the id of "cu
 var mainContainer = $(".container"); //Capture the div element with the class of "container".
 var header = $("header"); //Capture the whole header element.
 var body = $("body") //Capture the whole body element.
+
+//Function to display the updated time.
+function displayTime() { //Declare function.
+    var rightNow = dayjs().format("ddd DD MMM YYYY [at] HH:mm:ss"); //Capture the date and time (in a specific format) into a variable.
+    timeDisplayEl.text(rightNow); //Change the "text" property of the "timeDisplayEl" element to match the value of the "rightNow" variable.
+}
