917bbd4f9a134c351ae2df1e456fec51a4fd6bc7
Antimio <antimiocasimiro@gmail.com>
2024-01-23 23:07:23 +0000

    Use JQuery to capture the pertinent HTML elements that will be affected by the code.

---
+++ script.js
@@ -0,0 +1,5 @@
+//Use JQuery to capture the pertinent HTML elements that will be affected by the code.
+var timeDisplayEl = $("#currentDay"); //Capture the p element with the id of "currentDay".
+var mainContainer = $(".container"); //Capture the div element with the class of "container".
+var header = $("header"); //Capture the whole header element.
+var body = $("body") //Capture the whole body element.
