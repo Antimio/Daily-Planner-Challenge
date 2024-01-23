6ce7cf807cbda41985f48ac25f8acd799f7b87dc
Antimio <antimiocasimiro@gmail.com>
2024-01-23 23:16:15 +0000

    Declared a for loop that will, in effect, recreate the page every time the app is opened in the browser. The loop goes from 9 to 17, which represent the standard working-day hours.

--- script.js
+++ script.js
@@ -9,3 +9,10 @@ function displayTime() { //Declare function.
     var rightNow = dayjs().format("ddd DD MMM YYYY [at] HH:mm:ss"); //Capture the date and time (in a specific format) into a variable.
     timeDisplayEl.text(rightNow); //Change the "text" property of the "timeDisplayEl" element to match the value of the "rightNow" variable.
 }
+
+for (index = 9; index <= 17; index++) { //declare a for loop that will, in effect, recreate the page every time the app is opened in the browser. The loop goes from 9 to 17, which represent the standard working-day hours.
+    var timeBlock = $("<div>").attr("class", "time-block row"); //Use JQuery to create a new div element and then setting up class attributes to it that makes it connected to the pertinent css selectors in the style.css sheet. Store this into a variable.
+    var hour = $("<div>").attr("class", "col-2 col-sm-1 hour").text(index + ":00"); //Use JQuery to create a new div element and then setting up class attributes to it that makes it connected to the pertinent css selectors in the style.css sheet; and that also makes it responsive as per the bootstrap linkage. Change the text element to carry the current index and format it to show the time. Store this into a variable.
+    var textArea = $("<textarea>").attr("class", "col-2 col-sm-10 description"); //Use JQuery to create a new text-area element and then setting up class attributes to it that makes it connected to the pertinent css selectors in the style.css sheet; and that also makes it responsive as per the bootstrap linkage. Change the text element to carry the current index and format it to show the time. Store this into a variable.
+    var saveBtn = $("<button>").attr("class", "col-2 col-sm-1 saveBtn").append("<i class='fas fa-save'></i>"); //Use JQuery to create a new button element and then setting up class attributes to it that makes it connected to the pertinent css selectors in the style.css sheet; and that also makes it responsive as per the bootstrap linkage. Change the text element to carry the current index and format it to show the time. Append an "i" element that obtains an icon from Fontawsome. Store this into a variable.
+}
